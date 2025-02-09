2bfa75fa34bf29ddbbd813bd59d8f9a55c3714c5
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-09 19:54:36 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Sitemap',
+    '{description}' => 'Information for search engines about website pages',
+    '{permissions}' => [],
+
+    // Settings
+    '{settings.title}' => 'Module settings',
+    // Settings: поля
+    'Records in the file' => 'URL entries in the file',
+    'Maximum number of entries (articles and site sections) in a sitemap file' 
+        => 'Maximum number of entries (articles and site sections) in a sitemap file',
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Карта сайта',
+    '{description}' => 'Информация для поисковых систем о страницах веб-сайта',
+    '{permissions}' => [],
+
+    // Settings
+    '{settings.title}' => 'Настройка модуля',
+    // Settings: поля
+    'Records in the file' => 'Записей URL-адресов в файле',
+    'Maximum number of entries (articles and site sections) in a sitemap file' 
+        => 'Максимальное количество записей URL-адресов (статей и разделов сайта) в файле карты сайта',
+];
