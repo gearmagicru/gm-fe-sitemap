a78f355689f9acd865020927a8670f31ce07bb30
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-09 19:53:59 +0300

    Файл конфигурации настройки модуля

---
+++ config/.settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации настройки модуля.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c] 2015 Этот файл является частью модуля веб-приложения GearMagic. Web-студия
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'limit' => 1000 // записей в файле
+];
